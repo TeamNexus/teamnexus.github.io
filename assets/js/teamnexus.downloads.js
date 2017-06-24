ecd3aa53e06a5d74aececd0a808c9ee6449bad5e
Lukas0610 <mail@lukasberger.at>
2017-06-24 17:21:04 +0200

    re-worked style of download-page

--- assets/js/teamnexus.downloads.js
+++ assets/js/teamnexus.downloads.js
@@ -4,6 +4,8 @@
  * @author		Lukas Berger <https://lukasberger.at>
  * @license		GPL3
  */
+var DOWNLOADS = new Array();
+
 $(function() {
 
 	$.ajax({
@@ -16,7 +18,7 @@ $(function() {
 
 		var text = $('#ota-search').val().toLowerCase().split(' ');
 
-		$('.ota-search').each(function() {
+		$('tr[data-search]').each(function() {
 
 			var otaItem = $(this);
 			var otaSearchData = otaItem.attr('data-search');
@@ -29,7 +31,7 @@ $(function() {
 			}
 
 			if (hasMatched)
-				otaItem.css('display', 'inline-block');
+				otaItem.css('display', 'table-row');
 			else
 				otaItem.css('display', 'none');
 
@@ -60,6 +62,8 @@ function ota_ajax_done(data) {
 		if (data[device]['testing'] !== undefined)
 			ota_ajax_process_device_stage("testing", data[device]['testing']);
 	}
+
+	ota_ajax_show_devices();
 }
 
 function ota_ajax_process_device_stage(stage, data) {
@@ -68,54 +72,184 @@ function ota_ajax_process_device_stage(stage, data) {
 		var props     = data[rom]['props'];
 		var url       = data[rom]['url'];
 
-		var prop_board_platform               = props['ro.board.platform'];
-		var prop_build_data_utc               = parseInt(props['ro.build.date.utc']);
-		var prop_product_board                = props['ro.product.board'];
-		var prop_product_brand                = props['ro.product.brand'];
-		var prop_product_name                 = props['ro.product.name'];
-		var prop_chipname                     = props['ro.chipname'];
-		var prop_nexus_otarom                 = props['ro.nexus.otarom'];
-		var prop_build_version_release        = props['ro.build.version.release'];
-		var prop_build_version_security_patch = props['ro.build.version.security_patch'];
+		// they'll be process later, sort them by manufacturer and model first
+		var prop_product_brand = props['ro.product.brand'];
+		var prop_product_name  = props['ro.product.name'];
 
-		var build_time_diff = (Math.round(new Date() / 1000) - prop_build_data_utc);
-		var build_time_str  = "";
-		if (build_time_diff >= 31536000) {
-			build_time_str  = Math.floor(build_time_diff / 31536000);
-			build_time_str += " year" + (build_time_str == 1 ? "" : "s") + " ago";
-		} else if (build_time_diff >= 2073600) {
-			build_time_str  = Math.floor(build_time_diff / 2073600);
-			build_time_str += " month" + (build_time_str == 1 ? "" : "s") + " ago";
-		} else if (build_time_diff >= 86400) {
-			build_time_str  = Math.floor(build_time_diff / 86400);
-			build_time_str += " day" + (build_time_str == 1 ? "" : "s") + " ago";
-		} else if (build_time_diff >= 3600) {
-			build_time_str  = Math.floor(build_time_diff / 3600);
-			build_time_str += " hour" + (build_time_str == 1 ? "" : "s") + " ago";
-		} else if (build_time_diff >= 60) {
-			build_time_str  = Math.floor(build_time_diff / 60);
-			build_time_str += " minute" + (build_time_str == 1 ? "" : "s") + " ago";
-		} else {
-			build_time_str  = Math.floor(build_time_diff);
-			build_time_str += " second" + (build_time_str == 1 ? "" : "s") + " ago";
+		// store it
+		if (DOWNLOADS[prop_product_brand] === undefined)
+			DOWNLOADS[prop_product_brand] = new Array();
+
+		if (DOWNLOADS[prop_product_brand][prop_product_name] === undefined)
+			DOWNLOADS[prop_product_brand][prop_product_name] = new Array();
+
+		if (DOWNLOADS[prop_product_brand][prop_product_name][rom] === undefined)
+			DOWNLOADS[prop_product_brand][prop_product_name][rom] = new Array();
+
+		DOWNLOADS[prop_product_brand][prop_product_name][rom]['url'] = url;
+		DOWNLOADS[prop_product_brand][prop_product_name][rom]['stage'] = stage;
+		DOWNLOADS[prop_product_brand][prop_product_name][rom]['props'] = props;
+	}
+
+	DOWNLOADS = sortObject(DOWNLOADS);
+}
+
+function ota_ajax_show_devices() {
+	var html = "";
+	var sidebar = "";
+	var id = "";
+
+	// first, go through manufacturer
+	for (var manufacturer in DOWNLOADS) {
+		var models = DOWNLOADS[manufacturer];
+		var manufacturerSidebar = "";
+		models = sortObject(models);
+
+		// then through models
+		for (var model in models) {
+			var roms = models[model];
+			var modelSearchData = "";
+			var modelSidebar = "";
+			var modelHtml = "";
+			roms = sortObject(roms);
+
+			// and then the ROMs
+			for (var rom in roms) {
+				var data  = roms[rom];
+				var props = data['props'];
+				var stage = data['stage'];
+				var url   = data['url'];
+
+				var prop_board_platform               = props['ro.board.platform'];
+				var prop_build_data_utc               = parseInt(props['ro.build.date.utc']);
+				var prop_product_board                = props['ro.product.board'];
+				var prop_product_brand                = props['ro.product.brand'];
+				var prop_product_name                 = props['ro.product.name'];
+				var prop_chipname                     = props['ro.chipname'];
+				var prop_nexus_otarom                 = props['ro.nexus.otarom'];
+				var prop_build_version_release        = props['ro.build.version.release'];
+				var prop_build_version_security_patch = props['ro.build.version.security_patch'];
+
+				var build_time_diff = (Math.round(new Date() / 1000) - prop_build_data_utc);
+				var build_time_str  = "";
+				if (build_time_diff >= 31536000) {
+					build_time_str  = Math.floor(build_time_diff / 31536000);
+					build_time_str += " year" + (build_time_str == 1 ? "" : "s") + " ago";
+				} else if (build_time_diff >= 2073600) {
+					build_time_str  = Math.floor(build_time_diff / 2073600);
+					build_time_str += " month" + (build_time_str == 1 ? "" : "s") + " ago";
+				} else if (build_time_diff >= 86400) {
+					build_time_str  = Math.floor(build_time_diff / 86400);
+					build_time_str += " day" + (build_time_str == 1 ? "" : "s") + " ago";
+				} else if (build_time_diff >= 3600) {
+					build_time_str  = Math.floor(build_time_diff / 3600);
+					build_time_str += " hour" + (build_time_str == 1 ? "" : "s") + " ago";
+				} else if (build_time_diff >= 60) {
+					build_time_str  = Math.floor(build_time_diff / 60);
+					build_time_str += " minute" + (build_time_str == 1 ? "" : "s") + " ago";
+				} else {
+					build_time_str  = Math.floor(build_time_diff);
+					build_time_str += " second" + (build_time_str == 1 ? "" : "s") + " ago";
+				}
+
+				id = manufacturer.toLowerCase().replace(' ', '-') + '-' + 
+					model.toLowerCase().replace(' ', '-') + '-' +
+					rom.toLowerCase().replace(' ', '-');
+
+				var searchData = prop_nexus_otarom + ' ' + prop_product_brand + ' ' + prop_product_name + ' ' +prop_board_platform + ' ' + prop_product_board + ' ' +  prop_chipname;
+				var itemColor = (stage == "release" ? "success" : "warning");
+
+				if (modelSearchData == "") {
+					modelSearchData = searchData;
+				} else {
+					modelSearchData = prop_nexus_otarom + ' ' + modelSearchData;
+				}
+
+				modelHtml += '<tr id="' + id + '" data-search="' + searchData + '" data-spy="scroll">';
+				modelHtml += '	<th scope="row">';
+				modelHtml += '		<a href="' + url + '">' + prop_nexus_otarom + '</a>';
+				modelHtml += '	</th>';
+				// modelHtml += '	<td>' + prop_product_brand + ' ' + prop_product_name + '</td>';
+				modelHtml += '	<td>' + dateFormat(prop_build_data_utc * 1000, "dd.mm.yyyy, HH:MM:ss") + '</td>';
+				modelHtml += '	<td>' + prop_build_version_release + ' (Patch ' + prop_build_version_security_patch + ')</td>';
+				modelHtml += '</tr>';
+
+				modelSidebar += '<li class="nav-item"><a class="nav-link" href="#' + id + '">' + rom + '</a></li>';
+			}
+
+			id = manufacturer.toLowerCase().replace(' ', '-') + '-' + 
+				model.toLowerCase().replace(' ', '-');
+
+			html += '<tr id="' + id + '" class="thead-inverse" data-search="' + modelSearchData + '" data-spy="scroll">';
+			html += '	<th scope="row" colspan="3">' + manufacturer + ' ' + model + '</th>';
+			html += '</tr>';
+			html += modelHtml;
+
+			manufacturerSidebar += '<li class="nav-item">';
+			manufacturerSidebar += '	<a class="nav-link" href="#' + id + '">' + model + '</a>';
+			manufacturerSidebar += '	<ul class="nav nav-pills flex-column">' + modelSidebar + '</ul>';
+			manufacturerSidebar += '</li>';
 		}
 
-		var searchData = prop_nexus_otarom + ' ' + prop_product_brand + ' ' + prop_product_name + ' ' +prop_board_platform + ' ' + prop_product_board + ' ' +  prop_chipname;
-		var itemColor = (stage == "release" ? "success" : "warning");
-		var html = "";
-
-		html += '<a href="' + url + '" class="ota-search list-group-item list-group-item-action flex-column align-items-start list-group-item-' + itemColor + '" data-search="' + searchData + '">';
-		html += '	<div class="d-flex w-100 justify-content-between">';
-		html += '		<h5 class="mb-1">' + prop_nexus_otarom + ' for ' + prop_product_brand + ' ' + prop_product_name + '</h5>';
-		html += '		<small>' + build_time_str + '</small>';
-		html += '	</div>';
-		html += '	<p>';
-		html += '		<em class="fa fa-clock-o"></em> <strong>Build-Date:</strong> ' + dateFormat(prop_build_data_utc * 1000, "dd.mm.yyyy, HH:MM:ss") + '<br />';
-		html += '		<em class="fa fa-android"></em> <strong>Release:</strong> ' + prop_build_version_release + '<br />';
-		html += '		<em class="fa fa-shield"></em> <strong>Security-Patch:</strong> ' + prop_build_version_security_patch;
-		html += '	</p>';
-		html += '</a>';
-
-		$('#downloads').append(html);
+		sidebar += '<li class="nav-item">';
+		sidebar += '	<span class="nav-link">' + manufacturer + '</span>';
+		sidebar += '	<ul class="nav nav-pills flex-column">' + manufacturerSidebar + '</ul>';
+		sidebar += '</li>';
 	}
+
+	$('#downloads').append(html);
+	$('#sidebar').css({ 'top': $('body > .container').offset().top });
+	$('#sidebar > ul').append(sidebar);
+}
+
+function clone(obj) {
+    var copy;
+
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || "object" != typeof obj) return obj;
+
+    // Handle Date
+    if (obj instanceof Date) {
+        copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+        copy = [];
+        for (var i = 0, len = obj.length; i < len; i++) {
+            copy[i] = clone(obj[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+        copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
+}
+
+function sortObject(o) {
+    var sorted = {},
+    key, a = [];
+
+    for (key in o) {
+        if (o.hasOwnProperty(key)) {
+            a.push(key);
+        }
+    }
+
+    a.sort();
+
+    for (key = 0; key < a.length; key++) {
+        sorted[a[key]] = o[a[key]];
+    }
+    return sorted;
 }
